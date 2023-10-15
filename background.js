56cf87e235e6d4c18bb5b35abcb69475f69e3d27
Julian Elmasry <julian31186@gmail.com>
2023-10-15 17:57:59 -0400

    base version barely working

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
-browser.runtime.onMessage.addListener((message) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "getCookie") {
-        return browser.cookies.get({ url: message.url, name: message.name })
-            .then(cookie => {
-                return cookie;
-            })
-            .catch(error => {
-                console.error("Error fetching cookie:", error);
-                return null;
-            });
+        chrome.cookies.get({ url: message.url, name: message.name }, (cookie) => {
+            if (chrome.runtime.lastError) {
+                console.error("Error fetching cookie:", chrome.runtime.lastError);
+                sendResponse(null);
+            } else {
+                sendResponse(cookie);
+            }
+        });
+        return true; // indicates we will respond asynchronously
     }
 });
-
--- content.js
+++ content.js
@@ -1,20 +1,32 @@
-function colorDiv(slug) {
+function colorDiv(slug,count) {
     let element = document.querySelector(`a[data-contest-title-slug="${slug}"]`);
 
     if (element) {
         let grandparent = element.parentElement.parentElement;
-        grandparent.style.backgroundColor = 'black';
+        if (count == 4) {
+            grandparent.style.backgroundColor = '#143d11';
+        } else {
+            grandparent.style.backgroundColor = '#818739';
+        }
+        
     } else {
+        setTimeout(() => colorDiv(slug), 500);
         console.log('Element with the specified slug not found.');
     }
-
 }
 
-browser.runtime.sendMessage(
+function clearDivs() {
+    let coloredElements = document.querySelectorAll('div[style="background-color: black;"]');
+    coloredElements.forEach(element => {
+        element.style.backgroundColor = '';
+    });
+}
+
+chrome.runtime.sendMessage(
     { "action": "getCookie", "url": "https://leetcode.com", "name": "LEETCODE_SESSION" },
     (response) => {
-        if (browser.runtime.lastError) {
-            console.error("Error fetching cookie from background:", browser.runtime.lastError);
+        if (chrome.runtime.lastError) {
+            console.error("Error fetching cookie from background:", chrome.runtime.lastError);
         } else if (response) {
             console.log("Received cookie:", response.value);
             const cookie = 'LEETCODE_SESSION=' + response.value;
@@ -41,12 +53,28 @@ browser.runtime.sendMessage(
                 .then(data => {
                     console.log(data);
                     for (const o of data.data.allVirtualContestScoresPagified.data) {
-                        colorDiv(o.contest.titleSlug);
+                        colorDiv(o.contest.titleSlug,o.totalAcQuestions);
                     }
+                    localStorage.setItem('virtuals', JSON.stringify(data));
                 })
                 .catch(error => console.error('Error:', error));
         }
     }
 );
 
+document.body.addEventListener('click', (event) => {
+    let content = event.target.textContent.trim();
+    if (!isNaN(content) || content == "<" || content == ">") {
 
+        setTimeout(() => {console.log("Waiting for contests to load"),1000});
+
+        const storedData = localStorage.getItem('virtuals');
+        clearDivs();
+        if (storedData) {
+            const keyValueArray = JSON.parse(storedData);
+            for (const o of keyValueArray.data.allVirtualContestScoresPagified.data) {
+                colorDiv(o.contest.titleSlug);
+            }
+        }
+    }
+});
